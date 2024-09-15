7de02f7405b720155193b06ed15e2f15e1f1af20
Frank Ramirez <fkramirez@gmail.com>
2024-09-15 13:49:28 +0200

    [12.0] purchase_remove_company_views: commit inicial
    [12.0] purchase_remove_company_views: commit inicial

---
+++ purchase_remove_company_views/README.rst
@@ -0,0 +1,18 @@
+Metalesa - Purchase Remove Company Tree View
+============================================
+
+Remove company field from form and tree views of purchase order
+
+Credits
+=======
+
+Contributors
+------------
+
+* Visiion-Solutec (http://www.visiion.es)
+* Alejandro Granados <agranados@praxya.com>
+
+Maintainer
+----------
+
+This module is maintained by Praxya Inteligencia Tecnológica.
---
+++ purchase_remove_company_views/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Metalesa - Purchase Remove Company View",
+    "version": "12.0.0.2",
+    "author": "Visiion - Praxya",
+    "summary": "Remove company field from form and tree views of purchase order",
+    "license": "AGPL-3",
+    "category": 'Purchase',
+    'website': 'www.praxya.com',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'views/purchase.xml'
+    ],
+    'auto_install': True,
+}
