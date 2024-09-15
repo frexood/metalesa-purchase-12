2760cea09a23353c9adadaca058995925fbec2a2
Frank Ramirez <fkramirez@gmail.com>
2024-09-15 13:47:52 +0200

    [12.0] purchase_picking_status_filters: commit inicial
    [12.0] purchase_picking_status_filters: commit inicial

---
+++ purchase_picking_status_filters/README.rst
@@ -0,0 +1,18 @@
+Metalesa - Purchase Picking Filter Status
+=========================================
+
+Filters about picking state
+
+Credits
+=======
+
+Contributors
+------------
+
+* Jaime Valero <jvalero@praxya.com> (Migartion 8 -> 12)
+* Alejandro Granados <agranados@praxya.com> (Documentation)
+
+Maintainer
+----------
+
+This module is maintained by Praxya Inteligencia Tecnológica.
---
+++ purchase_picking_status_filters/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Metalesa - Purchase Picking Filter Status",
+    "summary": "Filters about picking state",
+    "version": "12.0.0.1",
+    "author": "Praxya",
+    "license": "AGPL-3",
+    "category": 'Purchase',
+    'website': 'www.praxya.com',
+    'depends': [
+        'purchase',
+        'purchase_picking_state'
+    ],
+    'data': [
+        'views/purchase.xml'
+    ],
+    'auto_install': True,
+}
