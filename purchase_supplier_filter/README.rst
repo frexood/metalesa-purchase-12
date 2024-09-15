2e4a97e0dc7b9fd8b2e48dee7a581414ea4fba77
Frank Ramirez <fkramirez@gmail.com>
2024-09-15 13:52:22 +0200

    [12.0] purchase_supplier_filter: commit inicial
    [12.0] purchase_supplier_filter: commit inicial

---
+++ purchase_supplier_filter/README.rst
@@ -0,0 +1,20 @@
+Metalesa - Purchase Supplier Filter
+===================================
+
+Filter the suppliers by company
+
+Credits
+=======
+
+Contributors
+------------
+
+* Visiion-Solutec (http://www.visiion.net)
+* Jaime Valero <jvalero@praxya.com> (Migartion 8 -> 12)
+* Alejandro Granados <agranados@praxya.com> (Documentation)
+
+
+Maintainer
+----------
+
+This module is maintained by Praxya Inteligencia Tecnológica.
---
+++ purchase_supplier_filter/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Metalesa - Purchase Supplier Filter",
+    "version": "12.0.0.1",
+    "author": "Praxya, Jaime Valero",
+    "summary": "Filter the suppliers by company",
+    "license": "AGPL-3",
+    "category": 'Purchase',
+    'website': 'www.praxya.com',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'views/purchase.xml'
+    ],
+    'auto_install': True,
+}
