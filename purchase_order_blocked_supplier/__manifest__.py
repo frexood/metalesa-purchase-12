99bea971c4fcd50d30c208802db19b19cc1045ce
Frank Ramirez <fkramirez@gmail.com>
2025-01-22 20:10:52 +0100

    [12.0][FEAT] purchase_order_blocked_supplir: proveedor bloqueado
    [12.0][FEAT] purchase_order_blocked_supplir: proveedor bloqueado

---
+++ purchase_order_blocked_supplier/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Metalesa - Purchase Blocked Supplier",
+    "version": "12.0.0.1",
+    "author": "Exood, " "Frank Ramirez",
+    "summary": "Add button Account Moves in Suppliers View",
+    "license": "AGPL-3",
+    "category": 'Product',
+    'website': '',
+    'depends': [
+        'purchase',
+        'user_permision',
+        'partner_custom_metalesa',
+    ],
+    'auto_install': True,
+}
